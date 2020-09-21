5447cee38737ea552dc2777b50c2c39b68ad00bc
ingalls <nick@ingalls.ca>
2020-09-21 09:01:39 -0700

    Add lambda builds

---
+++ upload-lambda.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# based on github.com/mapbox/lambda-cfn
+
+# ./util/upload-lambda lambda-directory bucket/prefix
+
+GITSHA=$(git rev-parse HEAD)
+REPO=$(basename $(git rev-parse --show-toplevel))
+echo "ok - ${GITSHA}"
+
+yarn install
+zip -qr /tmp/${GITSHA}.zip *
+
+aws s3 cp ./package.zip s3://devseed-artifacts/${REPO}/lambda-${GITSHA}.zip
+
+rm ./package.zip
