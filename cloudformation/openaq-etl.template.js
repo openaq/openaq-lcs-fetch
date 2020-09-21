b839f08382c713ec4f092f90f549c9533e7a6ea7
ingalls <nick@ingalls.ca>
2020-09-21 09:45:15 -0700

    Add runner function

--- cloudformation/openaq-etl.template.js
+++ cloudformation/openaq-etl.template.js
@@ -1,10 +1,38 @@
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
 const cf = require('@mapbox/cloudfriend');
 
+for (let source of fs.readdirSync(path.resolve(__dirname, '../sources/'))) {
+    source = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../sources/', source)))
+
+}
+
 const Parameters = {
+    GitSha: {
+        Type: 'String',
+        Description: 'Gitsha to Deploy'
+    }
 };
 
+const Resources = {
+    LambdaFetcher: {
+        Type: 'AWS::Lambda::Function',
+        Description: 'Fetch a single source for a given time period',
+        Code: {
+            S3Bucket: 'devseed-artifacts',
+            S3Key: cf.join(['openaq-etl/', cf.ref('GitSha')])
+        },
+        Handler: 'index.handler',
+        MemorySize: 128,
+        Runetime: 'nodejs12.x',
+        Timeout: '900'
+    }
+};
+
+
 module.exports = cf.merge({
-    Parameters
+    Parameters,
+    Resources
 });
