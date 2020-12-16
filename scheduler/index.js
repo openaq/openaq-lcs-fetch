a43646c5de77ac713f4dc8f1fe23dcbe0dde5e94
Anthony Lukach <anthonylukach@gmail.com>
2020-12-16 15:05:49 -0700

    Support async inserting into queue

--- scheduler/index.js
+++ scheduler/index.js
@@ -4,26 +4,22 @@ const AWS = require('aws-sdk');
 const sqs = new AWS.SQS();
 
 async function handler() {
-    if (!process.env.QUEUE)
-        throw new Error('QUEUE env var required');
+    if (!process.env.QUEUE_URL)
+        throw new Error('QUEUE_URL env var required');
     if (!process.env.SOURCES)
         throw new Error('SOURCES env var required');
 
-    const sources = process.env.SOURCES.split(',');
-    send(sources);
-}
-
-function send(sources) {
-    if (!sources.length) return;
-
-    sqs.sendMessage({
-        MessageBody: sources.pop(),
-        QueueUrl: process.env.QUEUE
-    }, (err) => {
-        if (err) console.error(err);
-
-        send(sources);
-    });
+    for (const source of process.env.SOURCES.split(',')) {
+        try {
+            await sqs.sendMessage({
+                MessageBody: source,
+                QueueUrl: process.env.QUEUE_URL
+            }).promise();
+            console.log(`Inserted '${source}' into queue`);
+        } catch (err) {
+            console.error(`Failed to send message for ${source}: ${err}`);
+        }
+    }
 }
 
 module.exports.handler = handler;
