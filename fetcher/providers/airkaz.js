663a0cf6c460456fe641ef49a41b903dd1cfe42f
Jordane Thomas <thomasjordane7@gmail.com>
2021-08-04 15:53:09 -0400

    starting provider script

---
+++ fetcher/providers/airkaz.js
@@ -0,0 +1,103 @@
+const Providers = require('../lib/providers');
+const { Sensor, SensorNode, SensorSystem } = require('../lib/station');
+const { Measures, FixedMeasure } = require('../lib/measure');
+const { Measurand } = require('../lib/measurand');
+const { fetchSecret, request } = require('../lib/utils');
+
+const lookup = {
+    // input_param: [measurand_parameter, measurand_unit]
+    'pm1.0': ['pm1', 'µg/m³'],
+    'pm2.5': ['pm25', 'µg/m³'],
+    'pm10.0': ['pm10', 'µg/m³'],
+    '0.3_um_count': ['um003', 'pp100ml'],
+    '0.5_um_count': ['um005', 'pp100ml'],
+    '1.0_um_count': ['um010', 'pp100ml'],
+    '2.5_um_count': ['um025', 'pp100ml'],
+    '5.0_um_count': ['um050', 'pp100ml'],
+    '10.0_um_count': ['um100', 'pp100ml'],
+    'humidity': ['humidity', '%'],
+    'temperature': ['temperature', 'f'],
+    'pressure': ['pressure', 'mb'],
+    'voc': ['voc', 'iaq'],
+    'ozone1': ['ozone', 'ppb']
+};
+
+async function processor(source_name, source) {
+    const [
+        measurands,
+        sensorReadings
+    ] = await Promise.all([
+        Measurand.getSupportedMeasurands(lookup),
+        fetchSecret(source.provider)
+            .then(({ apiKey }) => fetchSensorData(source, apiKey))
+    ]);
+
+    const stations = [];
+    const measures = new Measures(FixedMeasure);
+
+    console.log(`ok - pulled ${sensorReadings.length} stations`);
+
+    for (const reading of sensorReadings) {
+        const system = new SensorSystem();
+        const sensorNode = new SensorNode({
+            sensor_node_id: reading.sensor_index,
+            sensor_node_source_name: 'PurpleAir',
+            sensor_node_site_name: reading.name,
+            sensor_node_geometry: [reading.longitude, reading.latitude],
+            sensor_node_ismobile: false,
+            sensor_system: system
+        });
+
+        // For each measurement, add a sensor to the sensor system & log measurement
+        for (const measurand of measurands) {
+            const measure = reading[measurand.input_param];
+            if ([undefined, null].includes(measure)) continue;
+
+            const sensor = new Sensor({
+                sensor_id: `${sensorNode.sensor_node_source_name}-${reading.sensor_index}-${measurand.input_param}`,
+                measurand_parameter: measurand.parameter,
+                measurand_unit: measurand.normalized_unit
+            });
+
+            system.sensors.push(sensor);
+            measures.push(
+                new FixedMeasure({
+                    sensor_id: sensor.sensor_id,
+                    measure: measurand.normalize_value(measure),
+                    timestamp: reading.last_seen
+                })
+            );
+        }
+        // Upload sensor system
+        stations.push(
+            Providers.put_station(source_name, sensorNode)
+        );
+    }
+
+    await Promise.all(stations);
+    console.log(`ok - all ${stations.length} stations pushed`);
+
+    await Providers.put_measures(source_name, measures);
+    console.log(`ok - all ${measures.length} measurements pushed`);
+}
+
+async function fetchSensorData(source, apiKey) {
+    // https://airkaz.org/api/v0.1/?type=IQair&token=oBZAolofFdvhgjbduFaY
+    const url = new URL('/api/v0.1/', source.meta.url);
+    url.searchParams.append('type', 'IQair');
+    url.searchParams.append('token', 'oBZAolofFdvhgjbduFaY');
+
+    const { body: { fields, data } } = await request({
+        url: url,
+        json: true,
+        method: 'GET'
+    });
+
+    console.log(data);
+
+    return data;
+}
+
+module.exports = {
+    processor
+};
