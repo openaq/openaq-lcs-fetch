687b93d0244f22e384486a3fa5283219f96c091a
Christian Parker <c.andrew.parker@gmail.com>
2024-03-27 13:19:41 -0700

    Updated the schema and tests

--- test/source.test.js
+++ test/source.test.js
@@ -7,11 +7,6 @@ const ajv = new Ajv({
     schemaId: 'auto'
 });
 
-ajv.addMetaSchema(
-    require('ajv/lib/refs/json-schema-draft-07.json'),
-   'http://json-schema.org/draft-07/schema#'
-);
-
 const validate = ajv.compile(schema);
 
 tape('validate', (t) => {
@@ -19,13 +14,14 @@ tape('validate', (t) => {
     t.end();
 });
 
+
 // find all the sources, has to be synchronous for tape
 sources.forEach((source) => {
 	  tape(`tests for ${source}`, (t) => {
         try {
             const valid = validate(source);
 
-            t.ok(valid, `${source}: ${JSON.stringify(validate.errors)}`);
+            t.ok(valid, `${source.provider}: ${JSON.stringify(validate.errors)}`);
         } catch (err) {
             t.fail(`could not parse ${source} as JSON: ${err}`);
         }
